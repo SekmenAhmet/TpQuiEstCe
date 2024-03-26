6a9836ddbb79c2d903ab6ff652e2f9f6ebc9184c
onestsurgithubouuu <sekmenahmet04@gmail.com>
2024-03-26 08:52:31 +0100

    correction

--- index.php
+++ index.php
@@ -11,74 +11,72 @@
     
     <div class="img">
     <ul>
-    <li><img src="/Personnages/0000000.JPG" alt=""></li>
-    <li><img src="/Personnages/0001111.JPG" alt=""></li>
-    <li><img src="/Personnages/0010011.JPG" alt=""></li>
-    <li><img src="/Personnages/0011100.JPG" alt=""></li>
-    <li><img src="/Personnages/0100101.JPG" alt=""></li>
-    <li><img src="/Personnages/0101010.JPG" alt=""></li>
-    <li><img src="/Personnages/0110110.JPG" alt=""></li>
-    <li><img src="/Personnages/0111001.JPG" alt=""></li>
-    <li><img src="/Personnages/1000110.JPG" alt=""></li>
-    <li><img src="/Personnages/1001001.JPG" alt=""></li>
-    <li><img src="/Personnages/1010101.JPG" alt=""></li>
-    <li><img src="/Personnages/1011010.JPG" alt=""></li>
-    <li><img src="/Personnages/1100011.JPG" alt=""></li>
-    <li><img src="/Personnages/1101100.JPG" alt=""></li>
-    <li><img src="/Personnages/1110000.JPG" alt=""></li>
-    <li><img src="/Personnages/1111111.JPG" alt=""></li>
+    <?php 
+    $personnages = [
+        "0000000.JPG",
+        "0001111.JPG",
+        "0010011.JPG",
+        "0011100.JPG",
+        "0100101.JPG",
+        "0101010.JPG",
+        "0110110.JPG",
+        "0111001.JPG",
+        "1000110.JPG",
+        "1001001.JPG",
+        "1010101.JPG",
+        "1011010.JPG",
+        "1100011.JPG",
+        "1101100.JPG",
+        "1110000.JPG",
+        "1111111.JPG"
+    ];
+    foreach ($personnages as $value) {?>
+        <img src="Personnages/<?=$value?>">
+    <?php }?>
     </ul>
     </div>
 
     <!--QUESTIONS-->
     <div class="questions">
-
         <ul>
-
-        
             <li>Question 1</li>
             <h2 id="Q1">A-t-il des lunettes ?</h2>
-            <INPUT id="Q1" type="button" value="Oui" onclick="oui(0)">
-            <INPUT id="" type="button" value="Non" onclick="non(0)"><br>
+            <input type="button" value="Oui">
+            <input type="button" value="Non">
 
             <li>Question 2</li>
             <h2 id="Q2">A-t-il une moustache ?</h2>
-            <INPUT id="Q2" type="button" value="Oui" onclick="oui(0)">
-            <INPUT id="" type="button" value="non" onclick="non(0)">
+            <input type="button" value="Oui">
+            <input type="button" value="Non">
 
             <li>Question 3</li>
             <h2 id="Q3">A-t-il un chapeau ?</h2>
-            <INPUT id="Q3" type="button" value="oui" onclick="oui(0)">
-            <INPUT id="" type="button" value="Non" onclick="non(0)"><br>
+            <input type="button" value="Oui">
+            <input type="button" value="Non">
 
             <li>Question 4</li>
             <h2 id="Q4">A-t-il des cheveux ?</h2>
-            <INPUT id="Q4" type="button" value="Oui" onclick="oui(0)">
-            <INPUT id="" type="button" value="Non" onclick="non(0)"><br>
+            <INPUT type="button" value="Oui">
+            <INPUT type="button" value="Non">
 
             <li>Question 5</li>
             <h2 id="Q5">A-t-il une boucle d'oreille ?</h2>
-            <INPUT id="Q5" type="button" value="Oui" onclick="oui(0)">
-            <INPUT id="" type="button" value="Non" onclick="non(0)"><br>
+            <INPUT type="button" value="Oui">
+            <INPUT type="button" value="Non">
 
             <li>Question 6</li>
             <h2 id="Q6">A-t-il une barbe ?</h2>
-            <INPUT id="Q6" type="button" value="Oui" onclick="oui(0)">
-            <INPUT id="" type="button" value="Non" onclick="non(0)"><br>
+            <INPUT type="button" value="Oui">
+            <INPUT type="button" value="Non">
 
             <li>Question 7</li>
             <h2 id="Q7">A-t-il un noeud papillon ?</h2>
-            <INPUT id="Q7" type="button" value="Oui" onclick="oui(0)">
-            <INPUT id="" type="button" value="Non" onclick="non(0)"><br>
+            <input type="button" value="Oui">
+            <input type="button" value="Non">
 
         </ul>
     </div>
-
 <!--Reponse-->
-
-
-<li></li>
 
 </body>
 </html>
-
